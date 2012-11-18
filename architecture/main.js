3e6ade9327f71d4ddd031a55eec477f9171090c3
Ramil Karimov <ramil1017@gmail.com>
2012-11-18 21:05:06 +0400

    prototype for event core
    Initial commit.

---
+++ architecture/main.js
@@ -0,0 +1,31 @@
+/**
+ * Core of game. Includes only methods to work with plugins.
+ * @author Ramil_Karimov@epam.com
+ */
+var eventCore = (new function() {
+	var plugins = {};
+	var createMethodHandler = function (methodName) {
+		return function () {
+			for (i in plugins) {
+				if (plugins[i].methods && typeof plugins[i].methods[methodName] == 'function') {
+					plugins[i].methods[methodName].apply(this, arguments);
+				}
+			}
+		}
+	}
+	var registerMethods = function (plugin) {
+		if (plugin.methods) {
+			for (var methodName in plugin.methods) {
+				if (!this[methodName]) {
+					this[methodName] = createMethodHandler(methodName);
+				}
+			}
+		}
+	};
+	this.registerPlugins = function(plugin) {
+		plugins[plugin.name] = plugin;
+		registerMethods(plugin);
+		
+	};
+	return this;
+});
