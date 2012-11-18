441005ceb017c058a6aad56e51055a36a0f9e146
Ramil Karimov <ramil1017@gmail.com>
2012-11-18 23:34:27 +0400

    final commit for fully hand-made architecture
    later, may be, we'll return to it, but for hackaton better to use
    something more ready.

--- architecture/game.js
+++ architecture/game.js
@@ -3,21 +3,31 @@ game.registerPlugin({
 	name: "game",
 	entities: {
 		"game.engine": {
+			description: "this entity represents the hole engine",
+			methods: {
+				"initGameEngine": function() {
 
+				}
+			},
 			fires: {
 				"start": "start of game",
 				"step": "each step of game, when makes some desidions",
 				"fail": "level failed",
 				"win": "level finished"
-
 			}
 		}
-		"game.map": {
+		"game.menu": {
+			description: "game menu"
+		},
+		"game.level": {
+			description: "this entity represents one of levels"
 			
 		},
+		"game.map": {
+
+		},
 		"game.field": {
-
-
+			
 		}
 	},
 	methods: {
