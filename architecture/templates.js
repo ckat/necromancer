520c210dde503e16f41618d7e1e551d7a8082f16
Ramil Karimov <ramil1017@gmail.com>
2012-11-21 11:05:26 +0400

    added templates prototype
    removed unneded libs, added templates prototype

---
+++ architecture/templates.js
@@ -0,0 +1,22 @@
+/**
+ * collection "templates" contains aliases for checking functions.
+ */
+gameCore.templates["touch"] = function(obects) {
+
+};
+gameCore.templates["see"] = function(obects) {
+
+};
+gameCore.templates["wall"] = function(obects) {
+
+};
+gameCore.templates["deadlock"] = function(obects) {
+
+};
+gameCore.templates["type"] = function(obects) {
+
+};
+gameCore.templates["type_another"] = function(obects) {
+
+};
+
